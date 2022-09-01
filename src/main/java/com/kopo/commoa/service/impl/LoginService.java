ba3544385119cd420f0efc7e16542855df2cf15c
soyul94 <lokju1969@naver.com>
2022-09-01 19:05:43 +0900

    2022.09.01 19:05
    암호화 메소드

---
+++ src/main/java/com/kopo/commoa/service/impl/LoginService.java
@@ -0,0 +1,16 @@
+package com.kopo.commoa.service.impl;
+
+import java.security.MessageDigest;
+
+public interface LoginService {
+	
+	//고유아이디 로그인 처리
+	
+	//일반 로그인 처리
+	
+	//아이디 찾기
+	
+	//비밀번호 찾기(변경)
+	
+
+}
