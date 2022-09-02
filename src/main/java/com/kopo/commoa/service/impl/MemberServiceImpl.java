0666915e915655bf65cf5d5505285d3de30acf2b
lahyun02 <bethankful07@naver.com>
2022-09-02 18:08:48 +0900

    22년 9월 2일 18:08
    회원가입할때 비밀번호 암호화해서 db에 저장하기

--- src/main/java/com/kopo/commoa/service/impl/MemberServiceImpl.java
+++ src/main/java/com/kopo/commoa/service/impl/MemberServiceImpl.java
@@ -9,6 +9,8 @@ import com.kopo.commoa.mapper.MemberMapper;
 import com.kopo.commoa.service.MemberService;
 import com.kopo.commoa.vo.MemberVo;
 
+import utl.SHA256;
+
 @Service
 public class MemberServiceImpl implements MemberService {
 	
@@ -23,6 +25,13 @@ public class MemberServiceImpl implements MemberService {
 	//회원추가
 	@Override
 	public int memberInsert(MemberVo vo) {
+		
+		//비밀번호 암호화하기
+		String sha256 = SHA256.getSHA256(vo.getMemPassword());
+		 
+		//암호화된 비밀번호로 설정
+		vo.setMemPassword(sha256); 
+		
 		return memberMapper.memberInsert(vo); 
 	}
 
